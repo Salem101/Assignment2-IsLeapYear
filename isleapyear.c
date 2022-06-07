43886ecc0dae647688fffcde908a2d56e0ad2c16
Rivka Salem <55041326+Salem101@users.noreply.github.com>
2022-06-07 00:23:14 -0400

    Update isleapyear.c

--- isleapyear.c
+++ isleapyear.c
@@ -7,16 +7,14 @@ int main()
     printf("Enter year");
     scanf("%d", &year);
 
-    if((year/4)==0)
+    if((year%4)==0)
     {
 
-        if((year/100)!=0)
+        if((year%100)!=0) || ((year%400)==0)
         {
-            if((year/4000))
             {
-                    printf("%d%s", year, "was not a leap year");
-            }
             printf("%d%s", year, " was a leap year");
+            }
 
         }
     }
