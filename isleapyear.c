d3d2cefb1bb9e6c7a7bd06d91c4f061d18ac6ec6
Rivka Salem <55041326+Salem101@users.noreply.github.com>
2022-06-07 00:09:32 -0400

    Create isleapyear.c

---
+++ isleapyear.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int year;
+    printf("Enter year");
+    scanf("%d", &year);
+
+    if((year/4)==0)
+    {
+
+        if((year/100)!=0)
+        {
+            if((year/4000))
+            {
+                    printf("%d%s", year, "was not a leap year");
+            }
+            printf("%d%s", year, " was a leap year");
+
+        }
+    }
+    else{
+    printf("%d%s", year, " was not a leap year");
+    }
+    return 0;
+
+}
