3fd91387be05670e6ca2df819f6f6a59b8e401dd
Rivka Salem <55041326+Salem101@users.noreply.github.com>
2022-06-12 13:49:13 -0400

    Update isleapyear.c
    Trying to using atoi(argv[1]) to convert the string to integer

--- isleapyear.c
+++ isleapyear.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
 
-int main(int argc,char* argv[])
+int main(int argc,char** argv[])
 {
 
-    if(argv[1]%4==0 && (argv[1]%100 !=0 || argv[1]%400 == 0))
-    {
-        printf("%d%s", argv[1], " was a leap year");
-    }
+    if( atoi(argv[1])%4==0 && (atoi(argv[1])%100 !=0 || atoi(argv[1])%400 == 0))
+        printf("%d%s", atoi(argv[1]), " was a leap year");
     else {
-        printf("%d%s", argv[1], " was not a leap year");
+        printf("%d%s", atoi(argv[1]), " was not a leap year");
     }
     return 0;
 }
