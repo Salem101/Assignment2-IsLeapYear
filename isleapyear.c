8033cfdf57b575f3ebe80f65018d3213a378ab3b
Rivka Salem <55041326+Salem101@users.noreply.github.com>
2022-06-12 12:54:32 -0400

    Update isleapyear.c

--- isleapyear.c
+++ isleapyear.c
@@ -2,9 +2,7 @@
 
 int main(int argc,char* argv[])
 {
-    int counter;
-    printf("Program Name Is: %s",argv[0]);
-    if(argc%4==0 && (argc%100 !=0 || argc%400 == 0))
+    if(argc%4==0 && (argc%100 !=0 || argc%400 == 0)
     {
         printf("%d%s", argc, " was a leap year");
     }
